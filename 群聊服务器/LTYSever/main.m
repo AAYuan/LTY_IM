e2b3b98f04bf2092cb53c83e87406e542b381aec
AYuan <934309587@qq.com>
2016-04-25 15:26:13 +0800

    本demo在上一个服务器监听的基础上，可以开启多个终端模拟客户端，用本机作为服务器进行消息监听并转发

---
+++ 群聊服务器/LTYSever/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  服务器监听
+//
+//  Created by Vincent_Guo on 15/9/11.
+//  Copyright (c) 2015年 稳食哥 wechat:vg-ios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ServertListener.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        // 1.创建服务监听器
+        ServertListener *listener = [[ServertListener alloc] init];
+        
+        // 2.开始监听
+        [listener start];
+        
+        // 开启主运行循环
+        [[NSRunLoop mainRunLoop] run];
+    }
+    return 0;
+}
