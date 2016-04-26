77a2291669a09704744f6e410ff3ea9d9e2b0704
AYuan <934309587@qq.com>
2016-04-26 17:21:16 +0800

    开启异步线程登录

--- AYuanChat/AYuanChat/AppDelegate.m
+++ AYuanChat/AYuanChat/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "EMSDK.h"
 
 @interface AppDelegate ()
 
@@ -14,24 +15,33 @@
 
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    //AppKey:注册的appKey，详细见下面注释。
+    //apnsCertName:推送证书名(不需要加后缀)，详细见下面注释。
+    EMOptions *options = [EMOptions optionsWithAppkey:@"ayuan#ayuanchat"];
+//    options.apnsCertName = @"istore_dev";
+    [[EMClient sharedClient] initializeSDKWithOptions:options];
+    
     return YES;
 }
+
+// App进入后台
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    [[EMClient sharedClient] applicationDidEnterBackground:application];
+}
+
+// App将要从后台返回
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    [[EMClient sharedClient] applicationWillEnterForeground:application];
+}
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
--- AYuanChat/AYuanChat/ViewController.m
+++ AYuanChat/AYuanChat/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "EMSDK.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userName;
+@property (weak, nonatomic) IBOutlet UITextField *password;
 
 @end
 
@@ -16,7 +19,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+- (IBAction)registerBtn:(id)sender {
     
+}
+
+- (IBAction)loginBtn:(id)sender {
+
+    if (!(self.userName.text.length || self.password.text.length)) {
+        NSLog(@"请输入用户名或密码");
+    }else {//开启异步线程进行登录操作
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            EMError *error = [[EMClient sharedClient] loginWithUsername:self.userName.text password:self.password.text];
+            if (!error) {
+                NSLog(@"登陆成功");
+            }
+            
+        });
+    }
 }
 
 @end
